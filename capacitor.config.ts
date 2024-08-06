1ae01988b6a80a55dbb8eb39179913ed33579f00
hernandck1 <hernandck@msn.com>
2024-08-06 09:33:23 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'techtest.catacoly.hd',
+  appName: 'technical_test',
+  webDir: 'www'
+};
+
+export default config;
